888e61269ab120eb5899e2a3b86d869e0b39b415
mauro <figueroamauro.dev@gmail.com>
2024-12-16 20:48:55 -0300

    Test: Add test for "id" and "title" attributes

--- src/main/java/ar/com/old/challenge_literalura/models/validators/BookValidator.java
+++ src/main/java/ar/com/old/challenge_literalura/models/validators/BookValidator.java
@@ -1,4 +1,28 @@
 package ar.com.old.challenge_literalura.models.validators;
 
-public class BookValidator {
+public abstract class BookValidator {
+
+    public static Long validateId(Long id) {
+
+        if (id == null) {
+            throw new IllegalArgumentException("El id no puede ser nulo");
+        }
+        if (id < 0) {
+            throw new IllegalArgumentException("El id no puede ser negativo");
+        }
+
+        return id;
+    }
+
+    public static String validateTitle(String title) {
+
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("El título no puede estar vacío ni ser nulo");
+        }
+        if (title.length() >= 50) {
+            throw new IllegalArgumentException("El título no puede superar los 50 caracteres");
+        }
+
+        return title.trim();
+    }
 }
--- src/test/java/ar/com/old/challenge_literalura/models/validators/BookValidatorTest.java
+++ src/test/java/ar/com/old/challenge_literalura/models/validators/BookValidatorTest.java
@@ -1,6 +1,80 @@
 package ar.com.old.challenge_literalura.models.validators;
 
+import ar.com.old.challenge_literalura.models.Author;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ar.com.old.challenge_literalura.models.TestUtils.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
 public class BookValidatorTest {
 
 
+//    private String title;
+//    private final List<Author> authorList;
+//    private final List<String> languages;
+//    private int downloadCount;
+
+
+    @Nested
+    class idTest {
+
+        @ParameterizedTest
+        @NullSource
+        void shouldThrowException_whenIdIsNull(Long id) {
+            assertIllegalArgumentException(getValidateIdExecutable(id), "El id no puede ser nulo");
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = {-1L, -100L, -1000L})
+        void shouldThrowException_whenIdIsNegative(Long id) {
+            assertIllegalArgumentException(getValidateIdExecutable(id), "El id no puede ser negativo");
+        }
+
+
+    }
+
+    @Nested
+    class TitleTest {
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void shouldThrowException_whenTitleIsNullOrEmpty(String title) {
+            assertIllegalArgumentException(getValidateTitleExecutable(title), "El título no puede estar vacío ni ser nulo");
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {" 123123123123123123123123123123123123123123123123123123",
+                " test test test test test test test test test test test test test test test test test"})
+        void shouldThrowException_whenTitleIsLongerThan50Characters(String title) {
+            assertIllegalArgumentException(getValidateTitleExecutable(title), "El título no puede superar los 50 caracteres");
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "'test  ', 'test'",
+                "'  test', 'test'",
+                "'  testing  ', 'testing'"
+        })
+        void shouldTrimWhiteSpaces_whenNameContainSpaces(String values, String expected) {
+            assertEquals(expected, BookValidator.validateTitle(values));
+        }
+    }
+
+    private static Executable getValidateIdExecutable(Long id) {
+        return () -> BookValidator.validateId(id);
+    }
+
+    private static Executable getValidateTitleExecutable(String title) {
+        return () -> BookValidator.validateTitle(title);
+    }
 }

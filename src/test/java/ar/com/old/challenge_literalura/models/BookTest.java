50690fa4ec8eddf95814da2f8b4fac140de3fde1
mauro <figueroamauro.dev@gmail.com>
2024-12-20 19:55:39 -0300

    Test: Add test for setters in BookTest

--- src/test/java/ar/com/old/challenge_literalura/models/BookTest.java
+++ src/test/java/ar/com/old/challenge_literalura/models/BookTest.java
@@ -2,6 +2,7 @@ package ar.com.old.challenge_literalura.models;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -16,11 +17,43 @@ public class BookTest {
 
             assertEquals(1L, book.getId());
             assertEquals("test", book.getTitle());
-            assertEquals(100,book.getDownloadCount());
+            assertEquals(100, book.getDownloadCount());
             assertNotNull(book.getAuthorList());
             assertNotNull(book.getLanguages());
             assertTrue(book.getAuthorList().isEmpty());
             assertTrue(book.getLanguages().isEmpty());
+        }
+    }
+
+    @Nested
+    class SetterAndGetterTest {
+        Book book;
+
+        @BeforeEach
+        void init() {
+            book = new Book(1L, "test", 100);
+        }
+
+        @Test
+        void shouldSetId() {
+            book.setId(4L);
+
+            assertEquals(4L, book.getId());
+        }
+
+        @Test
+        void shouldSetTitle() {
+            book.setTitle("newTest");
+
+            assertEquals("newTest", book.getTitle());
+
+        }
+
+        @Test
+        void shouldSetDownloadCount() {
+            book.setDownloadCount(200);
+            assertEquals(200, book.getDownloadCount());
+
         }
     }
 

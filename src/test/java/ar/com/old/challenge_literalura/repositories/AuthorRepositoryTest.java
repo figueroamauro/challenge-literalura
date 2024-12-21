b8e80a5e52ed798fb83d15422ab4f7d753793b40
mauro <figueroamauro.dev@gmail.com>
2024-12-21 01:04:57 -0300

    Add: Create AuthorResositoryTest

---
+++ src/test/java/ar/com/old/challenge_literalura/repositories/AuthorRepositoryTest.java
@@ -0,0 +1,9 @@
+package ar.com.old.challenge_literalura.repositories;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class AuthorRepositoryTest {
+}

e6e2a589002f2e910d6dc4022e1cac3d1eb6611c
mauro <figueroamauro.dev@gmail.com>
2025-01-06 16:34:10 -0300

    Add: Add @Service anottation in BookService

--- src/main/java/ar/com/old/challenge_literalura/services/BookService.java
+++ src/main/java/ar/com/old/challenge_literalura/services/BookService.java
@@ -3,10 +3,12 @@ package ar.com.old.challenge_literalura.services;
 import ar.com.old.challenge_literalura.models.Book;
 import ar.com.old.challenge_literalura.repositories.BookRepository;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class BookService {
     private final BookRepository repository;
 
